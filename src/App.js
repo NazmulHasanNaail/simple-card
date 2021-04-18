fe6300eb0f0c8f9978eed7604e62a904abd0f349
Nazmul <nazmulhasanfci@gmail.com>
2021-04-18 15:18:36 +0600

    creat card app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './Card';
+import Sdata from './Sdata';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ let data = Sdata.map((Sdata)=>{
+     return <>
+        <Card 
+            imgSrc={Sdata.imgSrc} 
+            cat={Sdata.category}
+            title={Sdata.title}
+            btnLink={Sdata.link} 
+         /> 
+     </>
+ })
+ 
+function App(){
+    return<>
+      <h2 className="heading">List of top zee5 series 2021</h2>
+      {data}
+     </>
 }
-
 export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata =[
+{
+    imgSrc : 'https://akamaividz2.zee5.com/image/upload/w_716,h_403,c_scale,f_webp,q_auto:eco/resources/0-6-3221/list/063221_list_925411453.jpg',
+    category : 'Netflix original series',
+    title : 'Contract',
+    link : 'https://www.zee5.com/global/zee5originals/details/contract/0-6-3221'
+},
+{
+    imgSrc : 'https://akamaividz2.zee5.com/image/upload/w_716,h_403,c_scale,f_webp,q_auto:eco/resources/0-6-3217/list/063217_list_836351259.jpg',
+    category : 'zee5 original series',
+    title : 'Indian Pro Music League',
+    link : 'https://www.zee5.com/global/tvshows/details/indian-pro-music-league/0-6-3217'
+},
+{
+    imgSrc : 'https://akamaividz2.zee5.com/image/upload/w_716,h_403,c_scale,f_webp,q_auto:eco/resources/0-6-3190/list/063190_list.jpg',
+    category : 'zee5 original series',
+    title : 'Qubool Hai 2.0',
+    link : 'https://www.zee5.com/global/zee5originals/details/qubool-hai-20/0-6-3190'
+},
+{
+    imgSrc : 'https://akamaividz2.zee5.com/image/upload/w_716,h_403,c_scale,f_webp,q_auto:eco/resources/0-6-3121/list/063121_list.jpg',
+    category : 'zee5 original series',
+    title : 'Mithai',
+    link : 'https://www.zee5.com/global/tvshows/details/mithai/0-6-3121'
+},
+{
+    imgSrc : 'https://akamaividz2.zee5.com/image/upload/w_716,h_403,c_scale,f_webp,q_auto:eco/resources/0-6-366/list/06366_list_1246243718.jpg',
+    category : 'zee5 original series',
+    title : 'Kundali Bhagya',
+    link : 'https://www.zee5.com/global/tvshows/details/kundali-bhagya/0-6-366'
+}
+
+];
+
+export default Sdata;
